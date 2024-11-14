29543a09b302aec2126ce15c3c0d171d51983788
Devan Shepard <wdevanshepard@gmail.com>
2024-11-14 10:54:02 -0600

    Update 12-Route53.tf

--- 12-Route53.tf
+++ 12-Route53.tf
@@ -1,1 +1,14 @@
-
+data "aws_route53_zone" "mfnerd" {
+  name         = "mfnerd.org"  # The domain name you want to look up
+  private_zone = false
+}
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.mfnerd.zone_id
+  name    = "mfnerd.org"
+  type    = "A"
+  alias {
+    name                   = aws_lb.app1_alb.dns_name
+    zone_id                = aws_lb.app1_alb.zone_id
+    evaluate_target_health = true
+  }
+}
