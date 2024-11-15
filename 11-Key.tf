473684bd2e37fa35d0000356a28cb4fd874a498a
Devan Shepard <wdevanshepard@gmail.com>
2024-11-14 22:48:06 -0600

    Add files via upload

--- 11-Key.tf
+++ 11-Key.tf
@@ -1,1 +1,17 @@
+resource "tls_private_key" "linuxbox" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
 
+data "tls_public_key" "linuxbox" {
+  private_key_pem = tls_private_key.linuxbox.private_key_pem
+}
+
+output "private_key" {
+  value     = tls_private_key.linuxbox.private_key_pem
+  sensitive = true
+}
+
+output "public_key" {
+  value = data.tls_public_key.linuxbox.public_key_openssh
+}
--- 8-TargetGroup.tf
+++ 8-TargetGroup.tf
@@ -1,1 +1,51 @@
+resource "aws_lb_target_group" "app1_tg" {
+  name     = "app1-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.multi-app1.id
+  target_type = "instance"
 
+  health_check {
+    enabled             = true
+    interval            = 30
+    path                = "/"
+    protocol            = "HTTP"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 5
+    matcher             = "200"
+  }
+
+  tags = {
+    Name    = "App1TargetGroup"
+    Service = "App1"
+    Owner   = "User"
+    Project = "Web Service"
+  }
+}
+
+resource "aws_lb_target_group" "app2_tg" {
+  name     = "app2-target-group"
+  port     = 443
+  protocol = "HTTPS"
+  vpc_id   = aws_vpc.multi-app1.id
+  target_type = "instance"
+
+  health_check {
+    enabled             = true
+    interval            = 30
+    path                = "/"
+    protocol            = "HTTP"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 5
+    matcher             = "200"
+  }
+
+  tags = {
+    Name    = "App2TargetGroup"
+    Service = "App2"
+    Owner   = "User"
+    Project = "Web Service"
+  }
+}
